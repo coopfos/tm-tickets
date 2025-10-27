4c46b20721a76bb6aac17d93d1ea4e813b3c7eeb
Cooper Foster <cooper@whiteheadelectric.com>
2025-10-27 10:41:19 -0400

    Initial Commit

---
+++ TM Tickets/TM_TicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TM_TicketsApp.swift
+//  TM Tickets
+//
+//  Created by Cooper Foster on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TM_TicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
